3b5aeb8c4f5b4e532f56ded7b3addd5ec9929e0e
baidong.he <hebaidong1990@126.com>
2017-04-14 11:43:00 +0800

    增加displayname

--- HangfireDemo/Controllers/ExecuteController.cs
+++ HangfireDemo/Controllers/ExecuteController.cs
@@ -1,6 +1,7 @@
 ﻿using Hangfire;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,12 +14,17 @@ namespace HangfireDemo.Controllers
         public ActionResult Index()
         {
             //基于队列的任务处理
-            var jobId = BackgroundJob.Enqueue(() => Console.Write("zhangsan"));
+            var jobId = BackgroundJob.Enqueue(() => Con());
             //延迟任务执行
             var jobId2 = BackgroundJob.Schedule(() => Console.Write("延迟5分钟"), TimeSpan.FromMinutes(5));
             //定时任务
             RecurringJob.AddOrUpdate(() => Console.Write("定时任务"), Cron.Minutely);
             return View();
+        }
+        [DisplayName("输出")]
+        public void Con()
+        {
+            Console.Write("zhangsan");
         }
     }
 }
